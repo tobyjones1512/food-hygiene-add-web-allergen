55c532274910872ecd9b5bad3e7ba2d0288970e2
Toby Jones <33597254+tobyjones1512@users.noreply.github.com>
2024-10-23 00:50:04 +0100

    added link for coinbase wallet

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Rate My Selfies: Web App")
 
 st.write(
-    "Rate My Selfies uses Cryptocurrency (USDC) for transactions to reduce fees for both the developer and you, the user. Crypto also keeps payments secure, fast and private. If you do not have Cryptocurrency, we recommend Coinbase Wallet. It is secure, utilising PayPal and card payments for transactions; has a strong support team; and is free and easy to use."
+    "Rate My Selfies uses Cryptocurrency (USDC) for transactions to reduce fees for both the developer and you, the user. Crypto also keeps payments secure, fast and private. If you do not have Cryptocurrency, we recommend [Coinbase Wallet](https://www.coinbase.com/en-gb/wallet). It is secure, utilising PayPal and card payments for transactions; has a strong support team; and is free and easy to use."
 )
 
 st.warning('YOU MUST BE 18 OR OVER TO USE THIS PLATFORM.')

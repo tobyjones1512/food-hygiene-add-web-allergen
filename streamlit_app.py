b623fda3836580b2da3656a61a19506b592ea228
Toby Jones <33597254+tobyjones1512@users.noreply.github.com>
2024-10-23 00:54:53 +0100

    added 18 or over button

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 
+CAN_ACCESS = False
+
 st.title("Rate My Selfies: Web App")
 
-st.write(
-    "Rate My Selfies uses Cryptocurrency (USDC) for transactions to reduce fees for both the developer and you, the user. Crypto also keeps payments secure, fast and private. If you do not have Cryptocurrency, we recommend [Coinbase Wallet](https://www.coinbase.com/en-gb/wallet). It is secure, utilising PayPal and card payments for transactions; has a strong support team; and is free and easy to use."
-)
+if not CAN_ACCESS:
+    st.write(
+        "Rate My Selfies uses Cryptocurrency (USDC) for transactions to reduce fees for both the developer and you, the user. Crypto also keeps payments secure, fast and private. If you do not have Cryptocurrency, we recommend [Coinbase Wallet](https://www.coinbase.com/en-gb/wallet). It is secure, utilising PayPal and card payments for transactions; has a strong support team; and is free and easy to use."
+    )
 
-st.warning('YOU MUST BE 18 OR OVER TO USE THIS PLATFORM.')
+    st.warning('YOU MUST BE 18 OR OVER TO USE THIS PLATFORM.')
 
-st.caption('Neither "Rate My Selfies" nor its developer is sponsored by Coinbase. You are free to make your own informed choices.')
+    st.caption('Neither "Rate My Selfies" nor its developer is sponsored by Coinbase. You are free to make your own informed choices.')
 
-st.write(
-    'By continuing, you are also agreeing to the (EULA, Terms of Use and Privacy Policy)[https://ratemyselfies.xyz/wp-content/uploads/2024/10/Legal.pdf].'
-)
+    st.write(
+        'By continuing, you are also agreeing to the [EULA, Terms of Use and Privacy Policy](https://ratemyselfies.xyz/wp-content/uploads/2024/10/Legal.pdf).'
+    )
+
+    if st.button('I AM 18 OR OVER'):
+        CAN_ACCESS = True
+else:
+    st.heading("Dashboard")

a411fad5d9f20052d7533dde2c7e2d6ad93f0e89
Toby Jones <33597254+tobyjones1512@users.noreply.github.com>
2024-10-23 01:04:46 +0100

    sdfsef

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if not confirmed_age:
     StartScreen()
 
     if st.button('I AM 18 OR OVER', type="primary"):
-        st.write("")
         confirmed_age = True
+        st.write("")
 else:
     Dashboard()

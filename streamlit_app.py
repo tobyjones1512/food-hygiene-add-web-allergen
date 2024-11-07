6228426edc04b65df47a9fb1e16c1e6d50cd145d
Toby Jones <33597254+tobyjones1512@users.noreply.github.com>
2024-11-07 11:06:21 +0000

    addQueryParams

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 st.title("Food Hygiene Community Additions")
 
-st.write(st.secrets.DB_hostname)
+st.header(st.query_params["business"])
+
+if st.query_params["toAdd"] == "website":
+    url = st.text_input(placeholder="www.myfoodbusiness.co.uk")

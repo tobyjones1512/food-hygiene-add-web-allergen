469051e7c0f313e2cbee6376c96c70d5bbfd0823
Toby Jones <33597254+tobyjones1512@users.noreply.github.com>
2024-11-07 12:01:22 +0000

    added allergen guides

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,23 @@ if st.query_params["toAdd"] == "website":
                 st.success("Successfully added the website to our database. Thank you!")
 
                 st.balloons()
+
+elif st.query_params["toAdd"] == "allergenGuide":
+    url = st.text_input(label="Guide URL")
+
+    if len(url) > 5:
+        if st.button("Add Allergen Guide"):
+            s = pxssh.pxssh()
+            if not s.login (st.secrets.DB_hostname, st.secrets.DB_username, st.secrets.DB_password):
+                print ("SSH session failed on login.")
+            else:
+                print ("SSH session login successful")
+                s.sendline ('cd "New Website (July 2024)/' + st.secrets.DB_remote_dir + '/allergen-guides"')
+                s.prompt()
+                s.sendline ('echo "' + url + '" > "' + st.query_params["id"] + '.txt"')
+                s.prompt()
+                s.logout()
+
+                st.success("Successfully added the allergen guide to our database. Thank you!")
+
+                st.balloons()

d52107eca6ffb538ed7970885a7073ed42616b31
Toby Jones <33597254+tobyjones1512@users.noreply.github.com>
2024-11-07 11:14:18 +0000

    test requirement

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from pexpect import pxssh
 
 st.title("Food Hygiene Community Additions")
 
@@ -6,3 +7,7 @@ st.header(st.query_params["business"])
 
 if st.query_params["toAdd"] == "website":
     url = st.text_input(label="Website URL")
+
+    if len(url) > 5:
+        if st.button("Add Website"):
+            st.write("hi")
